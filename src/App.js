3557b8cc35143f28ea556d71f4f691108a08c2b2
thaovmogroup <thaottt@vmogroup.com>
2023-08-23 23:54:31 +0900

    ADD component ScrollToSectionNav

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ScrollToSectionNav from './scrollToViewSectionNavBar/scrollIntoView'; 
+
+
+function App() {
+  return (
+    <div className="App">
+     <ScrollToSectionNav/>
+    </div>
+  );
+}
+
+export default App;
