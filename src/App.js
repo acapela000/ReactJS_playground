a129f285f599062335f94a4f9f24194f9cdad65d
thao <mrtrados@gmail.com>
2023-09-06 01:09:58 +0900

    ADD SearchWordInText

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import ScrollToSectionNav from './scrollToViewSectionNavBar/scrollIntoView'; 
+import SearchWordInText from './searchWordInText/searchWordInText';
 
 
 function App() {
   return (
     <div className="App">
      <ScrollToSectionNav/>
+     <SearchWordInText/>
     </div>
   );
 }
