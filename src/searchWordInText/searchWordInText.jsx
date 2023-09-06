1ba9111fa06f67116cba0c6cfa3dade9e48628dd
thao <mrtrados@gmail.com>
2023-09-06 17:52:48 +0900

    UPDATE searchWordInText with DOMParser

--- src/searchWordInText/searchWordInText.jsx
+++ src/searchWordInText/searchWordInText.jsx
@@ -12,11 +12,15 @@ export default function SearchWordInText() {
             .then((res) => {
                 if (!res.ok) {
                     throw new Error("Network is not ok")
-                } else res.text();
+                } else return res.text();
             })
-            .then((text) => {
-               const wordCount = text.split(inputWord).length -1;
-                    setResult(wordCount);
+            .then((html) => {
+                const bodyContent = new DOMParser().parseFromString(html, "text/html").body.textContent;
+                console.log(html);
+                const regex = new RegExp(inputWord, "g");
+                const wordCount = (html.match(regex) || []).length;
+                //const wordCount = text.split(inputWord).length - 1;
+                setResult(wordCount);
             })
             .catch((error) => {
                 console.error("Fetching is error", error);
@@ -26,9 +30,12 @@ export default function SearchWordInText() {
 
     return (
         <div>
-            <input type="text" value={inputWord} placeholder="Input a word U wanna search in the website"
+            <h3>Search word in the script of a webpage</h3>
+            <input type="text" value={inputWord} style={{width: "300px"}}
+                placeholder="Input a word U wanna search in the website"
                 onChange={(e) => setInputWord(e.target.value)} />
-            <input type="text" value={url} placeholder="Enter a url of website"
+            <input type="text" value={url} style={{width: "300px"}}
+                placeholder="Enter a url of website"
                 onChange={(e) => setURL(e.target.value)} />
 
             {result > 0 ? (
